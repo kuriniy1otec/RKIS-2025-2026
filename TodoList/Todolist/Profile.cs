05a5c2715e52dd2b7c4f60d5500b390d386cf481
kuriniy1otec <vladdayneko07@mail.ru>
2025-10-24 23:48:03 +0300

    lesson 6/Создан класс Profile для хранения данных пользователя

---
+++ TodoList/Todolist/Profile.cs
@@ -0,0 +1,22 @@
+namespace Todolist
+{
+    public class Profile
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int BirthYear { get; private set; }
+
+        public Profile(string firstName, string lastName, int birthYear)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+        }
+
+        public string GetInfo()
+        {
+            int age = DateTime.Now.Year - BirthYear;
+            return $"{FirstName} {LastName}, возраст {age}";
+        }
+    }
+}
