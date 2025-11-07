41e76af3e3253bd2accb46268f60f3f9784218a6
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-08 01:55:35 +0300

    lesson 8/Добавлена загрузка данных при запуске программы

--- TodoList/Todolist/Program.cs
+++ TodoList/Todolist/Program.cs
@@ -4,19 +4,48 @@ namespace Todolist
     {
         private static TodoList todoList;
         private static Profile userProfile;
+        private static string dataDirectory;
+        private static string profileFilePath;
+        private static string todoFilePath;
 
         static void Main()
         {
-            InitializeUser();
-            InitializeTodoList();
+            InitializeFilePaths();
+            LoadData();
             RunTodoList();
         }
 
-        static void InitializeUser()
+        static void InitializeFilePaths()
         {
-            Console.WriteLine("Работу выполнили Дайнеко и Гергель 3832");
+            dataDirectory = Path.Combine(Directory.GetCurrentDirectory(), "data");
+            profileFilePath = Path.Combine(dataDirectory, "profile.txt");
+            todoFilePath = Path.Combine(dataDirectory, "todo.csv");
+        }
+
+        static void LoadData()
+        {
+            Console.WriteLine("Работу выполнили Гергель и Дайнеко 3832");
             Console.WriteLine();
 
+            FileManager.EnsureDataDirectory(dataDirectory);
+
+            userProfile = FileManager.LoadProfile(profileFilePath);
+            if (userProfile == null)
+            {
+                CreateNewProfile();
+            }
+            else
+            {
+                Console.WriteLine($"Загружен профиль: {userProfile.GetInfo()}");
+            }
+
+            todoList = FileManager.LoadTodos(todoFilePath);
+
+            Console.WriteLine();
+        }
+
+        static void CreateNewProfile()
+        {
             Console.Write("Введите имя: ");
             string firstName = Console.ReadLine();
 
@@ -27,8 +56,8 @@ namespace Todolist
             int birthYear = int.Parse(Console.ReadLine());
 
             userProfile = new Profile(firstName, lastName, birthYear);
-            Console.WriteLine($"Добавлен пользователь: {userProfile.GetInfo()}");
-            Console.WriteLine();
+            FileManager.SaveProfile(userProfile, profileFilePath);
+            Console.WriteLine($"Создан новый профиль: {userProfile.GetInfo()}");
         }
 
         static void InitializeTodoList()
@@ -51,4 +80,5 @@ namespace Todolist
         }
     }
 }
+
 
