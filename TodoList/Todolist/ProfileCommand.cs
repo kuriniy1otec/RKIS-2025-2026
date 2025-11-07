8274d17f14eb64fd971dd4aee95199bdbf0b154e
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-08 01:56:28 +0300

    lesson 8/ProfileCommand теперь сохраняет профиль при каждом вызове

--- TodoList/Todolist/ProfileCommand.cs
+++ TodoList/Todolist/ProfileCommand.cs
@@ -3,10 +3,17 @@
     public class ProfileCommand : ICommand
     {
         public Profile Profile { get; set; }
+        public string DataDirectory { get; set; }
 
         public void Execute()
         {
             Console.WriteLine(Profile.GetInfo());
+
+            if (!string.IsNullOrEmpty(DataDirectory))
+            {
+                string profileFile = Path.Combine(DataDirectory, "profile.txt");
+                FileManager.SaveProfile(Profile, profileFile);
+            }
         }
     }
 }
