5afeefafac2ae693a401aad9df63882e44259dd1
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-29 00:17:12 +0300

    lesson 10/Команды add, delete, update, status реализуют Unexecute

--- TodoList/Todolist/DeleteCommand.cs
+++ TodoList/Todolist/DeleteCommand.cs
@@ -4,26 +4,39 @@ namespace Todolist
 {
     public class DeleteCommand : ICommand
     {
-        public TodoList TodoList { get; set; }
         public int Index { get; set; }
+        private TodoItem _deletedItem;
+        private int _deletedIndex;
 
         public void Execute()
         {
             int taskIndex = Index - 1;
-            TodoItem item = TodoList.GetItem(taskIndex);
+            TodoItem item = AppInfo.Todos.GetItem(taskIndex);
             if (item != null)
             {
+                _deletedItem = item;
+                _deletedIndex = taskIndex;
                 string deletedText = item.Text;
-                TodoList.Delete(taskIndex);
+                AppInfo.Todos.Delete(taskIndex);
                 Console.WriteLine($"Задача удалена: {deletedText}");
+
+                string dataDir = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "data");
+                string todoFile = System.IO.Path.Combine(dataDir, "todo.csv");
+                FileManager.SaveTodos(AppInfo.Todos, todoFile);
             }
             else
             {
                 Console.WriteLine("Ошибка: неверный номер задачи");
             }
-            string dataDir = Path.Combine(Directory.GetCurrentDirectory(), "data");
-            string todoFile = Path.Combine(dataDir, "todo.csv");
-            FileManager.SaveTodos(TodoList, todoFile);
+        }
+
+        public void Unexecute()
+        {
+            if (_deletedItem != null)
+            {
+                AppInfo.Todos.Add(_deletedItem);
+                Console.WriteLine($"Восстановлена удаленная задача: {_deletedItem.Text}");
+            }
         }
     }
 }
