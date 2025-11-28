148739b3e2f8e5c23198ad7f6de538ab1e5f9ff5
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-29 00:05:47 +0300

    lesson 10/Создан статический класс AppInfo для глобального состояния

---
+++ TodoList/Todolist/AppInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Todolist
+{
+    public static class AppInfo
+    {
+        public static TodoList Todos { get; set; }
+        public static Profile CurrentProfile { get; set; }
+        public static Stack<ICommand> UndoStack { get; set; } = new Stack<ICommand>();
+        public static Stack<ICommand> RedoStack { get; set; } = new Stack<ICommand>();
+    }
+}
