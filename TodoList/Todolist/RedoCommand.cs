f88264a42827a2519591f43de2477035c4af32d7
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-29 00:20:53 +0300

    lesson 10/Добавлены команды undo и redo

---
+++ TodoList/Todolist/RedoCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Todolist
+{
+    public class RedoCommand : ICommand
+    {
+        public void Execute()
+        {
+            if (AppInfo.RedoStack.Count > 0)
+            {
+                ICommand command = AppInfo.RedoStack.Pop();
+                command.Execute();
+                AppInfo.UndoStack.Push(command);
+                Console.WriteLine("Повтор выполнена");
+            }
+            else
+            {
+                Console.WriteLine("Нет действий для повтора");
+            }
+        }
+
+        public void Unexecute()
+        {
+        }
+    }
+}
---
+++ TodoList/Todolist/UndoCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Todolist
+{
+    public class UndoCommand : ICommand
+    {
+        public void Execute()
+        {
+            if (AppInfo.UndoStack.Count > 0)
+            {
+                ICommand command = AppInfo.UndoStack.Pop();
+                command.Unexecute();
+                AppInfo.RedoStack.Push(command);
+                Console.WriteLine("Отмена выполнена");
+            }
+            else
+            {
+                Console.WriteLine("Нет действий для отмены");
+            }
+        }
+
+        public void Unexecute()
+        {
+        }
+    }
+}
