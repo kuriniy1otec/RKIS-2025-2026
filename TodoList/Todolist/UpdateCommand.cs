b052afea14779b6b5c2ca40e645c375e931ec943
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-29 00:17:49 +0300

    lesson 10/Команды add, delete, update, status реализуют Unexecute

--- TodoList/Todolist/UpdateCommand.cs
+++ TodoList/Todolist/UpdateCommand.cs
@@ -4,27 +4,39 @@ namespace Todolist
 {
     public class UpdateCommand : ICommand
     {
-        public TodoList TodoList { get; set; }
         public int Index { get; set; }
         public string NewText { get; set; }
+        private string _oldText;
+        private int _taskIndex;
 
         public void Execute()
         {
-            int taskIndex = Index - 1;
-            TodoItem item = TodoList.GetItem(taskIndex);
+            _taskIndex = Index - 1;
+            TodoItem item = AppInfo.Todos.GetItem(_taskIndex);
             if (item != null)
             {
-                string oldText = item.Text;
+                _oldText = item.Text;
                 item.UpdateText(NewText);
-                Console.WriteLine($"Задача обновлена: '{oldText}' -> '{NewText}'");
+                Console.WriteLine($"Задача обновлена: '{_oldText}' -> '{NewText}'");
+
+                string dataDir = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "data");
+                string todoFile = System.IO.Path.Combine(dataDir, "todo.csv");
+                FileManager.SaveTodos(AppInfo.Todos, todoFile);
             }
             else
             {
                 Console.WriteLine("Ошибка: неверный номер задачи");
             }
-            string dataDir = Path.Combine(Directory.GetCurrentDirectory(), "data");
-            string todoFile = Path.Combine(dataDir, "todo.csv");
-            FileManager.SaveTodos(TodoList, todoFile);
+        }
+
+        public void Unexecute()
+        {
+            TodoItem item = AppInfo.Todos.GetItem(_taskIndex);
+            if (item != null)
+            {
+                item.UpdateText(_oldText);
+                Console.WriteLine($"Отменено обновление: '{NewText}' -> '{_oldText}'");
+            }
         }
     }
 }
